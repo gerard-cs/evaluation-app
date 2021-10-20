e968bd7672894560f9e1ec48634ba40af05e904e
Gerard CS <gerard.csand@gmail.com>
2021-10-20 01:41:14 -0500

    Call seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(EvaluationSeeder::class );
+        $this->call(QuestionSeeder::class);
+        $this->call(AnswerSeeder::class);
     }
 }
